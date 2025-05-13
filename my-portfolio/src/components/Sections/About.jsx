e663e7fd7f801a2b1dd365a51a1d341ec4aefc75
BadrampalliShaikShaista <shaikshaista0806@gmail.com>
2025-05-13 21:54:27 +0530

    Initial commit

---
+++ my-portfolio/src/components/Sections/About.jsx
@@ -0,0 +1,112 @@
+import { RevealOnScroll } from "../RevealOnScroll";
+
+export const About = () => {
+  const frontendSkills = [
+    "html",
+    "CSS",
+    "JavaScript",
+    "React",
+    "TailwindCSS",
+    "Bootstrap",
+  ];
+
+  const backendSkills = ["Node.js", "Python", "MySQL", "MongoDB", "Express.js"];
+
+  return (
+    <section
+      id="about"
+      className="min-h-screen flex items-center justify-center py-20"
+    >
+      <RevealOnScroll>
+        <div className="max-w-3xl mx-auto px-4">
+          <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
+            {" "}
+            About Me
+          </h2>
+
+          <div className="rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all">
+            <p className="text-gray-300 mb-6">
+              As an enthusiastic and fast learner with a passion for problem-solving, I eagerly take on task always
+              striving to exceed expectations. My commitment to continuous learning ensures
+              that I stay updated with the latest industry trends and technologies, making me a valuable asset to any
+              team. 
+            </p>
+
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+              <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                <h3 className="text-xl font-bold mb-4"> Frontend</h3>
+                <div className="flex flex-wrap gap-2">
+                  {frontendSkills.map((tech, key) => (
+                    <span
+                      key={key}
+                      className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
+                                    hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition
+                    "
+                    >
+                      {tech}
+                    </span>
+                  ))}
+                </div>
+              </div>
+
+              <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                <h3 className="text-xl font-bold mb-4"> Backend</h3>
+                <div className="flex flex-wrap gap-2">
+                  {backendSkills.map((tech, key) => (
+                    <span
+                      key={key}
+                      className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
+                                    hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition
+                    "
+                    >
+                      {tech}
+                    </span>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8">
+            <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
+              <h3 className="text-xl font-bold mb-4"> 🏫 Education </h3>
+              <ul className="list-disc list-inside text-gray-300 space-y-2">
+                <li>
+                  <strong> B.Tech in Electrical and Electronics Engineering</strong> - National Institute of Technology Calicut
+                  (2022-2026)
+                </li>
+                <li>
+                  Relevant Coursework: Data Structures and Algorithms, Web Development, Database Management Systems
+                </li>
+              </ul>
+            </div>
+            <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
+              <h3 className="text-xl font-bold mb-4"> Work Experience</h3>
+              <div className="space-y-4 text-gray-300">
+                <div>
+                  <h4 className="font-semibold">
+                    {" "}
+                    SkinCare recommendation Website  {" "}
+                  </h4>
+                  <p>
+                    website that recommends skincare products based on user input.
+                  </p>
+                </div>
+
+                <div>
+                  <h4 className="font-semibold">
+                    {" "}
+                    Blinker bot  
+                  </h4>
+                  <p>
+                    A bot which mimics the human eye. 
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </RevealOnScroll>
+    </section>
+  );
+};
---
+++ my-portfolio/src/components/Sections/Projects.jsx
@@ -0,0 +1,178 @@
+ import { RevealOnScroll } from "../RevealOnScroll";
+
+export const Projects = () => {
+  return (
+    <section
+      id="projects"
+      className="min-h-screen flex items-center justify-center py-20"
+    >
+      <RevealOnScroll>
+        <div className="max-w-5xl mx-auto px-4">
+          <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
+            {" "}
+            Featured Projects
+          </h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+              <h3 className="text-xl font-bold mb-2"> SkinCare Recommendation Website </h3>
+              <p className="text-gray-400 mb-4">
+               Developed a full-stack skincare recommendation website using
+               React.js and MERN stack , integrating a machine learning model to
+               analyze user skin concerns and suggest personalized products.
+               Implemented secure login/signup authentication and linked product
+               suggestions to online platforms for direct purchases. Designed
+               responsive pages including Home, About, Services, Contact, and user
+               authentication flows. 
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["React", "mongo.db", "Machine Learning", "Express.js", "Node.js"].map((tech, key) => (
+                  <span
+                    key={key}
+                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
+                                    hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all
+                    "
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+
+              <div className="flex justify-between items-center">
+                <a
+                  href="#"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Project →
+                </a>
+              </div>
+            </div>
+            <div
+              className="
+              glass p-6 rounded-xl border border-white/10 
+              hover:-translate-y-1 hover:border-blue-500/30
+              hover:shadow-[0_4px_20px_rgba(59,130,246,0.1)]
+              transition-all
+            "
+            >
+              <h3 className="text-xl font-bold mb-2">Blinker Bot</h3>
+              <p className="text-gray-400 mb-4">
+                Developed a human-eye mimicking "Blinker Bot" using Arduino Uno
+                and ESP32 , focusing on servo motor control for eye movement and
+                blinking. Programmed eye and blink movements using Arduino IDE ,
+                and integrated joystick control for real-time motion handling.
+                Collaborated on hardware integration , enabling lifelike
+                responsiveness through precise servo coordination. 
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["Python", "TensorFlow", "D3.js", "Flask"].map((tech, key) => (
+                  <span
+                    key={key}
+                    className="
+                      bg-blue-500/10 text-blue-500 py-1 px-3 
+                      rounded-full text-sm
+                      transition
+                      hover:bg-blue-500/20 hover:-translate-y-0.5
+                      hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]
+                    "
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+              <div className="flex justify-between items-center">
+                <a
+                  href="#"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Project →
+                </a>
+              </div>
+            </div>
+
+            <div
+              className="
+              glass p-6 rounded-xl border border-white/10 
+              hover:-translate-y-1 hover:border-blue-500/30
+              hover:shadow-[0_4px_20px_rgba(59,130,246,0.1)]
+              transition-all
+            "
+            >
+              <h3 className="text-xl font-bold mb-2">ai agent Chatbot</h3>
+              <p className="text-gray-400 mb-4">
+               Designed and developed an AI-powered chatbot using GPT to understand user queries, 
+               store and retrieve contextual and structured data (e.g., task assignments, user info) 
+               with long-term memory. Integrated MongoDB for dynamic memory storage and React.js for 
+               a user-friendly front-end interface. 
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["React.js", "Python(FastAPI/Flask)", "OpenAI GPT-4", "MongoDB Atlas(Cloud-hosted)", "n8n"].map(
+                  (tech) => (
+                    <span
+                      key={tech}
+                      className="
+                      bg-blue-500/10 text-blue-500 py-1 px-3 
+                      rounded-full text-sm
+                      transition
+                      hover:bg-blue-500/20 hover:-translate-y-0.5
+                      hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]
+                    "
+                    >
+                      {tech}
+                    </span>
+                  )
+                )}
+              </div>
+              <div className="flex justify-between items-center">
+                <a
+                  href="#"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Project →
+                </a>
+              </div>
+            </div>
+
+            <div
+              className="
+              glass p-6 rounded-xl border border-white/10 
+              hover:-translate-y-1 hover:border-blue-500/30
+              hover:shadow-[0_4px_20px_rgba(59,130,246,0.1)]
+              transition-all
+            "
+            >
+              <h3 className="text-xl font-bold mb-2">Portfolio</h3>
+              <p className="text-gray-400 mb-4">
+               Built a modern, responsive personal portfolio website to showcase projects, skills, and contact information. 
+               Designed responsive website using TailwindCSS and React.js for dynamic UI rendering and smooth user experience. 
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["React.js", "TailwindCSS", "Git"].map((tech, key) => (
+                  <span
+                    key={key}
+                    className="
+                      bg-blue-500/10 text-blue-500 py-1 px-3 
+                      rounded-full text-sm
+                      transition
+                      hover:bg-blue-500/20 hover:-translate-y-0.5
+                      hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]
+                    "
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+              <div className="flex justify-between items-center ">
+                <a
+                  href="#"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Project →
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </RevealOnScroll>
+    </section>
+  );
+};
